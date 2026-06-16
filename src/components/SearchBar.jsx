3bea34137783aa8e5dfc4f8d17b1e6431fe1e04e
Roshan Sinhabhu <roshansinhabahu@gmail.com>
2026-06-16 17:32:52 +0545

    Refactor SearchBar component styles and integrate react-query for game search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 import './SearchBar.css'
+import { useQuery } from '@tanstack/react-query';
 
 const SearchBar = () => {
     const [input,setInput] = useState('');
+    console.log(input);
 
+    const { data,isLoading,error} = useQuery({
+        queryKey: ["search-game"],
+        queryFn: async() => {
+            const res = await fetch(`https://api.rawg.io/api/games?key=1df2aae67f0f4e34bb2d6b8d53f5f06b&search=${input}`);
+        }
+    })
     return(
         <div className='search-container'>
-            <input className='input-field' 
+            <input className='input' 
             placeholder='Search Games...'
             onChange={(e)=>{setInput(e.target.value)}}
             value={input}/>
