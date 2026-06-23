61c2152962927b92b921f93faf40ff280286e1a4
Roshan Sinhabhu <roshansinhabahu@gmail.com>
2026-06-23 11:15:20 +0530

    Refactor SearchBar component; update styles and functionality for improved user interaction

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 import './SearchBar.css'
 import { useQuery } from '@tanstack/react-query';
+import { IoSearch } from 'react-icons/io5';
 
 const SearchBar = ({ onSearch,value,onFocusScroll }) => {
+    const [active,setActive] = useState(false);
+
+    const handleToggle = () => {
+        setActive(prev => !prev);
+    }
 
     return(
-        <div className='search-container'>
-            <input className='input' 
+        <div className={`search-container ${active ? 'active' : ''}`}>
+            <button className='search-icon' onClick={handleToggle} onFocus={onFocusScroll}><IoSearch /></button>
+            <input className='search-input'
             placeholder='Search Games...'
             onChange={(e)=>{onSearch(e.target.value)}}
             value={value}
-            onFocus={onFocusScroll}/>
+            />
         </div>
     )
 }
